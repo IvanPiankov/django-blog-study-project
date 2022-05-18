0db62cad69dde4bae9ac7c4941ff95c5e20af8d4
IvanPiankov <vanypyankov@gmail.com>
2022-05-18 22:50:45 +0300

    Added models Article, StudyOrganization, Specialisation

--- blogposting/models.py
+++ blogposting/models.py
@@ -3,10 +3,28 @@ from django.db import models
 
 
 class StudyOrganization(models.Model):
-    pass
+    ORGANIZATION_UN = "U"
+    ORGANIZATION_SH = "S"
+    ORGANIZATION_COLL = "S"
+
+    STATUS_CHOICES = [
+        (ORGANIZATION_UN, "University"),
+        (ORGANIZATION_SH, "School"),
+        (ORGANIZATION_COLL, "College"),
+    ]
+    organization_name = models.CharField(max_length=150,
+                                         primary_key=True)
+    type_study_organization = models.CharField(max_length=3,
+                                               choices=STATUS_CHOICES,
+                                               default=ORGANIZATION_SH)
+
 
 class Specialisation(models.Model):
-    pass
+    specialisation_name = models.CharField(max_length=150,
+                                           primary_key=True)
+    study_time = models.IntegerField()
+    specialisation_study_organization = models.ManyToManyField(StudyOrganization, blank=True)
+
 
 class Author(models.Model):
     # lite variant we can do chooses object with chooses object in Django documentations
@@ -29,16 +47,23 @@ class Author(models.Model):
 
     bio = models.TextField(blank=True)
 
-    study_organizations = models.ManyToManyField(StudyOrganization,
-                                                 max_length=200,
-                                                 default="",
-                                                 blank=True)
-    specialisation = models.ManyToManyField(Specialisation,
-                                            max_length=200,
-                                            default="",
-                                            blank=True)
+    study_organizations = models.ManyToManyField(StudyOrganization, blank=True)
+    specialisation = models.ManyToManyField(Specialisation, blank=True)
 
     def __str__(self):
         return f" Author <{self.user}>"
 
 
+class Article(models.Model):
+    author = models.ForeignKey(Author,
+                               on_delete=models.PROTECT)
+
+    title = models.CharField(max_length=150)
+    text = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(null=True,
+                                        blank=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Article(title={self.title!r}, author={self.author}"
