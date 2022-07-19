4fab0124a78804e5a5dbcd7f9abe9bdf526da538
IvanPiankov <vanypyankov@gmail.com>
2022-07-19 22:25:25 +0300

    WIP

--- blogposting/tests/test_models.py
+++ blogposting/tests/test_models.py
@@ -1,4 +1,10 @@
+import pytest
+
+from blogposting.models import Specialisation
 
 
-def test_model():
-    pass
+@pytest.mark.django_db
+def test_create_specialisation():
+    specialisation = Specialisation(specialisation_name="Test_1", study_time=10)
+    specialisation.save()
+    assert Specialisation.objects.count() == 1
