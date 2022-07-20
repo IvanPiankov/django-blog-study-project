4a94a24e506fb26067dec48e35a63a504a7ea635
IvanPiankov <vanypyankov@gmail.com>
2022-07-20 16:19:49 +0300

    add admin function and superuser

--- blogposting/admin.py
+++ blogposting/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blogposting.models import (Specialisation, StudyOrganization, Author, Article, Course, Student)
+
+admin.site.register(Author)
+admin.site.register(Specialisation)
+admin.site.register(StudyOrganization)
+admin.site.register(Article)
+admin.site.register(Course)
+admin.site.register(Student)
