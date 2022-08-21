a6d2cc47b40ed5802408c4ed1bf48826f16a970e
IvanPiankov <vanypyankov@gmail.com>
2022-08-21 20:34:04 +0300

    Created ModelViewSet for Author model

--- blogposting/routers.py
+++ blogposting/routers.py
@@ -1,7 +1,8 @@
-# TODO: Create serilalizer
+
 # for many to many (many=True)
 from rest_framework import routers
-from blogposting.api_views import CourseViewSet
+from blogposting.api_views import CourseViewSet, AuthorViewSet
 
 app_router = routers.DefaultRouter()
-app_router.register(r'restcourse', CourseViewSet)
+app_router.register(r'api-course', CourseViewSet)
+app_router.register(r'api-author', AuthorViewSet)
